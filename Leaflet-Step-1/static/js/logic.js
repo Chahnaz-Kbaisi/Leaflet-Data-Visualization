a4d03e4e0bb977b1ed6efdb1f7cb471f9de9e55c
Chahnaz-Kbaisi <chahnaz.k@comcast.net>
2021-01-02 20:04:57 -0500

    Adding map layers

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -22,17 +22,94 @@ var myMap = L.map("map", {
 L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
     maxZoom: 18,
+    // id: "mapbox/streets-v11s",
     id: "mapbox/streets-v11",
     accessToken: API_KEY
 }).addTo(myMap);
 
-// Load in geojson data
+// Storing API query variable
+// Storing the geojson data url for all earthquakes from the past 7 days
 var geoData = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
-// var geojson;
 
-// Grab the data with d3
+// Grabbing the geoData with d3
 d3.json(geoData).then(function (data) {
-    console.log(geoData);
+    // console.log(geoData);
+
+
+    // Your data markers should reflect the magnitude of the earthquake in their size and color. 
+    // Earthquakes with higher magnitudes should appear larger and darker in color.
+
+    // Creating a function that will determine color of the marker as the "magnitude" is high or lower
+    function choosemagColor(magnitude) {
+        switch (magnitude) {
+            case magnitude >= 5:
+                return "red";
+            case magnitude >= 4:
+                return "darkorange";
+            case magnitude >= 3:
+                return "gold";
+            case magnitude >= 2:
+                return "green";
+            case magintude >= 1:
+                return "lightgreen";
+            default:
+                return "lightyellow";
+        };
+    };
+    // Creating a function to determin the size of the marker as the "magnitude" is higher or lower
+    function choosemagSize(magintude) {
+        switch (magnitude) {
+            case magnitude >= 5:
+                return 30;
+            case magnitude >= 4:
+                return 24;
+            case magnitude >= 3:
+                return 18;
+            case magnitude >= 2:
+                return 12;
+            case magintude >= 1:
+                return 6;
+            default:
+                return 3;
+        };
+    };
+
+    // Creating a function to determine the the style of the marker as the "magnitude" is higher or lower
+
+    function stylemag(feature) {
+        return {
+            opacity: 1,
+            fillColor: choosemagColor(feature.properties.mag),
+            color: "Black",
+            radius: choosemagSize(feature.properties.mag),
+            stroke: true,
+            weight: 0.75
+
+        };
+    };
+
+    // Creating a layer of GeoJSON data to contain the array of the created features
+    var geoJsonLayer = L.geoJSON(data, {
+        pointToLayer: function (feature, latlng) {
+            return L.circleMarker(latlng);
+        },
+        // Adding market style created
+        style: stylemag,
+
+        // Binding a Popup for the location, date, time, and the magnitude of the earthquake
+        onEachFeature: function (feature, layer) {
+            layer.bindPopup(
+                "<h4>Location: " + feature.properties.place + "</h4><hr><p>Date/Time: " +
+                new Date(feature.properties.time) + "</p><hr><p>Magnitude: " +
+                feature.properties.mag + "</p>"
+            );
+        }
+
+
+
+
+    }).addTo(myMap);
+
 
 
 });
