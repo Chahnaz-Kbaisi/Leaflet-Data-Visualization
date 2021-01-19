e911765446c14fe2fd8c278f5ea8b2c46410c696
Chahnaz-Kbaisi <chahnaz.k@comcast.net>
2021-01-19 11:43:21 -0500

    Adding tile layers

--- Leaftlet-Step-2/static/js/logic.js
+++ Leaftlet-Step-2/static/js/logic.js
@@ -9,34 +9,78 @@
 // that can be turned on and off independently.
 // * Add layer controls to our map.
 
-// Creat map Object
-var myMap = L.map("map", {
-    center: [40.7, -94.5],
-    zoom: 4
-});
 
 // Storing the geojson data and the tectonic plates
 var geoData = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
-var tectonicPlates = "https://github.com/fraxen/tectonicplates/blob/master/GeoJSON/PB2002_boundaries.json";
+var tectonicplateData = "";
+
+var tectonicPlates = new L.LayerGroup();
+var earthquakes = new L.LayerGroup();
 
 // Adding tile layers
-var statellite = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+var satellite = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 512,
     maxZoom: 18,
-    id: "satellite-v9",
+    zoomOffset: -1,
+    id: "mapbox/satellite-v9",
     accessToken: API_KEY
-}).addTo(myMap);
+});
 
-var scale = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 512,
     maxZoom: 18,
-    id: "light-v10",
+    zoomOffset: -1,
+    id: "mapbox/light-v10",
     accessToken: API_KEY
-}).addTo(myMap);
+});
 
 var outdoors = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 512,
     maxZoom: 18,
-    id: "outdoors-v11",
+    zoomOffset: -1,
+    id: "mapbox/outdoors-v11",
     accessToken: API_KEY
-}).addTo(myMap);
+});
+
+// Creating map Object
+var myMap = L.map("map", {
+    center: [
+        40.7, -94.5
+    ],
+    zoom: 4,
+    layers: [satellite, streetmap, outdoors]
+});
+
+// Adding satellite tile layer to myMap
+satellite.addTo(myMap)
+
+// Adding streetmap tile layer to myMap
+streetmap.addTo(myMap)
+
+// Adding outdoors tile layer to myMap
+outdoors.addTo(myMap)
+
+
+var baseMaps = {
+    Satellite: satellite,
+    StreetMap: streetmap,
+    Outdoor: outdoors
+};
+
+var overlays = {
+    "Tectonic Plates": tectonicPlates,
+    Earthquakes: earthquakes
+};
+
+L.control.layers(baseMaps, overlays).addTo(myMap);
+
+d3.json(geoData, function (data) {
+
+});
+
+d3.json(tectonicplateData, function (data) {
+
+});
